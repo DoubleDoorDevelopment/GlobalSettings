29cce955eecbdbaf3e012cdd038a979b7c1ea995
Claycorp <Claycorp@doubledoordev.net>
2021-10-04 01:54:30 -0500

    Fix for servers crashing.

--- src/main/java/net/doubledoordev/globalsettings/GlobalSettings.java
+++ src/main/java/net/doubledoordev/globalsettings/GlobalSettings.java
@@ -4,35 +4,49 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 
 
 @Mod("globalsettings")
 public class GlobalSettings
 {
-     static final String MOD_ID = "globalsettings";
+    static final String MOD_ID = "globalsettings";
 
     static Logger LOGGER = LogManager.getLogger();
 
-     static Utils util = new Utils();
+    static Utils util;
 
     public GlobalSettings()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+        {
+            // Because I'm too fucking lazy to do this proper because forge had to make a mess of client only mods because stupid.
+            if (FMLEnvironment.dist == Dist.CLIENT)
+            {
+                // Register the setup method for modloading
+                FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+                // Register the doClientStuff method for modloading
+                FMLJavaModLoadingContext.get().getModEventBus().addListener(this::handleConfigFile);
+                // Register ourselves for server and other game events we are interested in
+                MinecraftForge.EVENT_BUS.register(this);
+            }
+
+            ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        }
     }
 
     //PreInit
     private void setup(final FMLCommonSetupEvent event)
     {
+        util = new Utils();
         util.setMasterFile();
         // We need to make sure the options.txt file is there before we try to write to it because fresh instances don't have it.
         if (!util.vanillaSettings.exists())
@@ -49,7 +63,7 @@ public class GlobalSettings
     }
 
     // Must be done in InterModEnqueueEvent or the sounds aren't there yet to be set.
-    private void doClientStuff(final InterModEnqueueEvent event)
+    private void handleConfigFile(final InterModEnqueueEvent event)
     {
         // Do our magic if we have a master file and auto load is enabled. Otherwise just make one.
         if (util.checkMasterFile())
@@ -62,6 +76,7 @@ public class GlobalSettings
                 util.replaceVanillaOptions();
                 GlobalSettings.LOGGER.info("Loaded Global options!");
             }
-        } else util.makeMaster();
+        }
+        else util.makeMaster();
     }
 }
